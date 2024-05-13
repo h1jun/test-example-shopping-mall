249d59aaecc2b8534973a4f29c3c39a0147e1f5a
Wonjun <dittoavec@gmail.com>
2024-05-13 16:25:28 +0900

    feat: 4.6. RTL 비동기 유틸 함수를 통한 노출 테스트 작성

--- src/pages/home/components/tests/ProductList.spec.jsx
+++ src/pages/home/components/tests/ProductList.spec.jsx
@@ -27,11 +27,65 @@ vi.mock('react-router-dom', async () => {
   };
 });
 
-it('로딩이 완료된 경우 상품 리스트가 제대로 모두 노출된다', async () => {});
+it('로딩이 완료된 경우 상품 리스트가 제대로 모두 노출된다', async () => {
+  // Arrange
+  await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
 
-it('보여줄 상품 리스트가 더 있는 경우 show more 버튼이 노출되며, 버튼을 누르면 상품 리스트를 더 가져온다.', async () => {});
+  // 1초 동안 50ms마다 요소가 있는지 조회하고 결과를 반환한다. 재시도 시간은 옵션으로 설정 가능
+  const productCards = await screen.findAllByTestId('product-card');
 
-it('보여줄 상품 리스트가 없는 경우 show more 버튼이 노출되지 않는다.', async () => {});
+  // Assert
+  expect(productCards).toHaveLength(PRODUCT_PAGE_LIMIT);
+
+  // 상품 카드의 상품명, 카테고리, 가격, 장바구니 구매 버튼이 렌더링 되는지 확인
+  productCards.forEach((el, index) => {
+    const productCard = within(el);
+    const product = data.products[index];
+
+    expect(productCard.getByText(product.title)).toBeInTheDocument();
+    expect(productCard.getByText(product.category.name)).toBeInTheDocument();
+    expect(
+      productCard.getByText(formatPrice(product.price)),
+    ).toBeInTheDocument();
+    expect(
+      productCard.getByRole('button', { name: '장바구니' }),
+    ).toBeInTheDocument();
+    expect(
+      productCard.getByRole('button', { name: '구매' }),
+    ).toBeInTheDocument();
+  });
+});
+
+it('보여줄 상품 리스트가 더 있는 경우 show more 버튼이 노출되며, 버튼을 누르면 상품 리스트를 더 가져온다.', async () => {
+  // Arrange
+  const { user } = await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
+
+  // showMore 버튼의 노출 여부는 api에서 상품 목록 데이터를 가져와 렌더링 된 후에 알 수 있다.
+  // show more 버튼의 노출 여부를 정확하게 판단하기 위해
+  // findBy 쿼리를 사용하여 먼저 첫 페이지에 해당하는 상품 목록이 렌더링되는 것을 기다려야 함
+  await screen.findAllByTestId('product-card');
+
+  expect(screen.getByRole('button', { name: 'Show more' })).toBeInTheDocument();
+
+  const moreBtn = screen.getByRole('button', { name: 'Show more' });
+  await user.click(moreBtn);
+
+  // 상품의 개수가 10개인지 확인
+  expect(await screen.findAllByTestId('product-card')).toHaveLength(
+    PRODUCT_PAGE_LIMIT * 2,
+  );
+});
+
+it('보여줄 상품 리스트가 없는 경우 show more 버튼이 노출되지 않는다.', async () => {
+  // Arrange, 모킹 데이터 20개보다 많은 수 50으로 limit을 설정
+  await render(<ProductList limit={50} />);
+
+  // findBy 쿼리를 사용하여 먼저 첫 페이지에 해당하는 상품 목록이 렌더링되는 것을 기다려야 함
+  await screen.findAllByTestId('product-card');
+
+  // getByText는 결과가 없으면 Error를 던지고, queryByText는 결과가 없으면 null을 반환한다.
+  expect(screen.queryByText('Show more')).not.toBeInTheDocument();
+});
 
 describe('로그인 상태일 경우', () => {
   beforeEach(() => {
@@ -39,6 +93,9 @@ describe('로그인 상태일 경우', () => {
   });
 
   it('구매 버튼 클릭시 addCartItem 메서드가 호출되며, "/cart" 경로로 navigate 함수가 호출된다.', async () => {
+    // 통합 테스트 역시 좀 더 큰 범위로 비즈니스 로직을 검증할 수 있지만,
+    // 이처럼 다른 페이지의 로직(실제 장바구니에 추가되는 로직)을 검증할 수는 없기 때문에 이러한 모킹 작업이 필요
+    // - addCartItemFn 함수를 모킹하여 사용
     const addCartItemFn = vi.fn();
     mockUseCartStore({ addCartItem: addCartItemFn });
 
@@ -62,6 +119,7 @@ describe('로그인 상태일 경우', () => {
   });
 
   it('장바구니 버튼 클릭시 "장바구니 추가 완료!" toast를 노출하며, addCartItem 메서드가 호출된다.', async () => {
+    // Arrange
     const addCartItemFn = vi.fn();
     mockUseCartStore({ addCartItem: addCartItemFn });
 
@@ -72,10 +130,12 @@ describe('로그인 상태일 경우', () => {
     // 첫번째 상품을 대상으로 검증한다.
     const productIndex = 0;
     const product = data.products[productIndex];
+    // Act
     await user.click(
       screen.getAllByRole('button', { name: '장바구니' })[productIndex],
     );
 
+    // Assert
     expect(addCartItemFn).toHaveBeenNthCalledWith(1, product, 10, 1);
     expect(
       screen.getByText(`${product.title} 장바구니 추가 완료!`),
@@ -84,6 +144,7 @@ describe('로그인 상태일 경우', () => {
 });
 
 describe('로그인이 되어 있지 않은 경우', () => {
+  // user store에 대한 모킹을 하지 않으면 기본적으로 비로그인 상태이기 때문에 별도 setup 함수 없이 바로 테스트를 실행할 수 있다.
   it('구매 버튼 클릭시 "/login" 경로로 navigate 함수가 호출된다.', async () => {
     const { user } = await render(<ProductList limit={PRODUCT_PAGE_LIMIT} />);
 
